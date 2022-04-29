ce418160f9cdcdab64ce5df21c4da1c578eb480d
brunosana <brunosa@dcomp.ufs.br>
2022-04-28 21:00:44 -0300

    feat: add explosion particle on SpaceInvaders enemy

--- Assets/Scenes/Games/Space-Invaders/Assets/Scripts/SpaceInvaders_EnemyController.cs
+++ Assets/Scenes/Games/Space-Invaders/Assets/Scripts/SpaceInvaders_EnemyController.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class SpaceInvaders_EnemyController : MonoBehaviour
 {
 
+    [SerializeField] public Transform enemy_explosion;
+    public Transform currentEnemyPosition;
+
+    private void Start() {
+        currentEnemyPosition = gameObject.transform;
+    }
+
     private void OnTriggerEnter2D(Collider2D collisor) {
         Debug.Log(collisor.name);
         if(collisor.name == "shot(Clone)"){
+            Instantiate(enemy_explosion, currentEnemyPosition.position, currentEnemyPosition.rotation);
             Destroy(gameObject, 0f);
         }
     }
